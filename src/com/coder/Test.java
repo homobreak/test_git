44bc5de8c97ef299976af38b8c203b12c027b63f
anluo <peryzx@163.com>
2023-08-05 20:18:13 +0800

    第三次提交

--- src/com/coder/Test.java
+++ src/com/coder/Test.java
@@ -9,5 +9,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello git");
+        System.out.println("hello idea");
     }
 }
