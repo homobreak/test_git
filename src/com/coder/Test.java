a5a3a83599e6306f2fb9dfe2c86ca25e63eb324a
homobreak <106607336+homobreak@users.noreply.github.com>
2023-08-05 23:22:32 +0800

    Update Test.java

--- src/com/coder/Test.java
+++ src/com/coder/Test.java
@@ -13,5 +13,6 @@ public class Test {
         System.out.println("hello git");
         System.out.println("hello fix branch");
         System.out.println("hello github");
+        System.out.println("hello github web");
     }
 }
